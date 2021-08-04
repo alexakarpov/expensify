965a4d3f62a1596a5a3f5fd6c2479c58406a120e
Alexandre Karpov <alexakarpov@gmail.com>
2021-08-04 18:35:58 -0400

    hello component and reformat

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+import Hello from './components/Hello'
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+ReactDOM.render(<Hello />, document.getElementById('root'))
