bc61045608ecee86ce84211ecaa9aa18f3640c36
Alexandre Karpov <alexakarpov@gmail.com>
2021-08-04 22:00:17 -0400

    pair of dummy routes

--- src/app.js
+++ src/app.js
@@ -3,5 +3,16 @@ import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import Hello from './components/Hello'
+import Create from './components/Create'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-ReactDOM.render(<Hello />, document.getElementById('root'))
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path='/' exact={true} component={Hello} />
+      <Route path='/create' component={Create} />
+    </div>
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('root'))
