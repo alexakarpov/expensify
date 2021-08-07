377e58c03137a2388dea2e53bf8676cd7ee6d6f0
Alexandre Karpov <alexakarpov@gmail.com>
2021-08-06 23:34:59 -0400

    render details of an expense, started

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const EditExpensePage = () => <h1>Edit Expense</h1>
+const EditExpensePage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>Edit Expense</h1>
+      <p>with id of {props.match.params.id}</p>
+    </div>
+  )
+}
 
 export default EditExpensePage
