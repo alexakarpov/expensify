879bd2e005c29c0cc88526cf41585b1339b7280f
Alexandre Karpov <alexakarpov@gmail.com>
2021-08-08 00:46:41 -0400

    subscribe and unsubscribe

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -14,25 +14,22 @@ const store = createStore((state = { count: 0 }, action) => {
   console.log('store creation - ', action)
   switch (action.type) {
     case 'INCREMENT':
-      console.log('increment')
       return { count: state.count + 1 }
     case 'DECREMENT':
-      console.log('decrement')
       return { count: state.count - 1 }
     case 'RESET':
-      console.log('reset')
       return { count: 0 }
     default:
       return state
   }
 })
-store.dispatch(action1)
-store.dispatch(action1)
-store.dispatch(action1)
-console.log(store.getState())
 
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch(action1)
+store.dispatch(action1)
+store.dispatch(action1)
 store.dispatch(action2)
-console.log(store.getState())
-
 store.dispatch(action3)
-console.log(store.getState())
