e8694cbcb1133d960aed3cfde8971eba8fe513cf
Alexandre Karpov <alexakarpov@gmail.com>
2021-08-08 01:06:01 -0400

    more redux play

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,22 +1,18 @@
 import { createStore } from 'redux'
 
-let action1 = {
-  type: 'INCREMENT',
-}
-let action2 = {
-  type: 'DECREMENT',
-}
-let action3 = {
-  type: 'RESET',
+let makeAction = (type, value) => {
+  return { type, value: value ? value : 1 }
 }
 
 const store = createStore((state = { count: 0 }, action) => {
   console.log('store creation - ', action)
   switch (action.type) {
     case 'INCREMENT':
-      return { count: state.count + 1 }
+      return { count: state.count + action.value }
     case 'DECREMENT':
-      return { count: state.count - 1 }
+      return { count: state.count - action.value }
+    case 'SET':
+      return { count: action.value }
     case 'RESET':
       return { count: 0 }
     default:
@@ -28,8 +24,8 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState())
 })
 
-store.dispatch(action1)
-store.dispatch(action1)
-store.dispatch(action1)
-store.dispatch(action2)
-store.dispatch(action3)
+store.dispatch(makeAction('INCREMENT'))
+store.dispatch(makeAction('INCREMENT', 10))
+store.dispatch(makeAction('DECREMENT'))
+store.dispatch(makeAction('RESET'))
+store.dispatch(makeAction('SET', 333))
