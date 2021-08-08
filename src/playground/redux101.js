e05c5b2f6482af05b556c3ff682078ae692a3ae6
Alexandre Karpov <alexakarpov@gmail.com>
2021-08-08 00:40:52 -0400

    without reducers

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,9 +1,38 @@
 import { createStore } from 'redux'
 
-const store = createStore((state = { count: 0 }) => {
-  return state
+let action1 = {
+  type: 'INCREMENT',
+}
+let action2 = {
+  type: 'DECREMENT',
+}
+let action3 = {
+  type: 'RESET',
+}
+
+const store = createStore((state = { count: 0 }, action) => {
+  console.log('store creation - ', action)
+  switch (action.type) {
+    case 'INCREMENT':
+      console.log('increment')
+      return { count: state.count + 1 }
+    case 'DECREMENT':
+      console.log('decrement')
+      return { count: state.count - 1 }
+    case 'RESET':
+      console.log('reset')
+      return { count: 0 }
+    default:
+      return state
+  }
 })
+store.dispatch(action1)
+store.dispatch(action1)
+store.dispatch(action1)
+console.log(store.getState())
 
-let s = store.getState()
+store.dispatch(action2)
+console.log(store.getState())
 
-console.log(s)
+store.dispatch(action3)
+console.log(store.getState())
