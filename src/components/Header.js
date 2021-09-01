1d8c021b37bfe37a6abf8cc13365e1b7b2f25975
Alexandre Karpov <alexakarpov@gmail.com>
2021-09-01 17:58:40 -0400

    drop one of the navlinks

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,9 +13,6 @@ const Header = () => (
     <NavLink to='/create' activeClassName='is-active'>
       Create
     </NavLink>
-    <NavLink to='/edit' activeClassName='is-active'>
-      Edit
-    </NavLink>
   </header>
 )
 
