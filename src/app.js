cc01c30455cf5e027d151e138852654256dab2a5
Alexandre Karpov <alexakarpov@gmail.com>
2021-09-01 20:58:50 -0400

    fix all the imports

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import configureStore from './store/configureStore'
 import AppRouter from './routers/AppRouter'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore()
 store.dispatch(addExpense({ description: 'Water bill' }))
 store.dispatch(addExpense({ description: 'Gas bill' }))
-store.dispatch(setTextFilter('water'))
+store.dispatch(setTextFilter('bill'))
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
