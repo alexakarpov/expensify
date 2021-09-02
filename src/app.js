76aa3a84fae278991473dcc68590c5f5ef7f00d5
Alexandre Karpov <alexakarpov@gmail.com>
2021-09-02 12:17:56 -0400

    forgot the provider; added another expense

--- src/app.js
+++ src/app.js
@@ -7,16 +7,26 @@ import AppRouter from './routers/AppRouter'
 import { addExpense } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
+import { Provider } from 'react-redux'
 
 const store = configureStore()
-store.dispatch(addExpense({ description: 'Water bill' }))
-store.dispatch(addExpense({ description: 'Gas bill' }))
-store.dispatch(setTextFilter('bill'))
+store.dispatch(addExpense({ description: 'Water bill', amount: 50 }))
+store.dispatch(addExpense({ description: 'Gas bill', amount: 90 }))
+store.dispatch(addExpense({ description: 'Electric bill', amount: 40 }))
+store.dispatch(setTextFilter('gas'))
+
+// setTimeout(() => {
+//   store.dispatch(setTextFilter('water'))
+// }, 3000)
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 
-const jsx = <AppRouter />
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+)
 
 ReactDOM.render(jsx, document.getElementById('root'))
