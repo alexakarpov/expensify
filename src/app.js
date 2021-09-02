852a2299b8d81971f0334a80af6e68c394a83695
Alexandre Karpov <alexakarpov@gmail.com>
2021-09-01 20:21:38 -0400

    back to app.js, now with manually added expenses

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ import configureStore from './store/configureStore'
 import AppRouter from './routers/AppRouter'
 
 const store = configureStore()
+store.dispatch(addExpense({ description: 'Water bill' }))
+store.dispatch(addExpense({ description: 'Gas bill' }))
+store.dispatch(setTextFilter('water'))
 
-console.log(store.getState())
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
+
 ReactDOM.render(AppRouter, document.getElementById('root'))
