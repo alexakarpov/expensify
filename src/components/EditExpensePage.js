87020515237c00501a2c5bd71f8461a22c695b6a
Alexandre Karpov <alexakarpov@gmail.com>
2021-09-03 18:59:53 -0400

    expense form and two components using it

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import ExpenseForm from './ExpenseForm'
 
-const EditExpensePage = (props) => {
-  console.log(props)
-  return <div>Editing the expense with id of {props.match.params.id}</div>
-}
+const EditExpensePage = (props) => (
+  <div>
+    <h2>the expense with id of {props.match.params.id}</h2>
+    <ExpenseForm></ExpenseForm>
+  </div>
+)
 
 export default EditExpensePage
