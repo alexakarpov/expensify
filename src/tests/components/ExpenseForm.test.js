3c83fda65065e3ec1bd024851e19e01639abf107
Alexandre Karpov <alexakarpov@gmail.com>
2021-09-10 09:43:57 -0400

    a few more tests

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import ExpenseForm from '../../components/ExpenseForm'
 import expenses from '../fixtures/expenses'
+import moment from 'moment'
 
 test('renders the ExpenseForm without data', () => {
   const wrapper = shallow(<ExpenseForm></ExpenseForm>)
@@ -22,4 +23,71 @@ test('renders error for an invalid form submission', () => {
   })
   expect(wrapper.state('error').length).toBeGreaterThan(0)
   expect(wrapper).toMatchSnapshot()
+})
+
+test('Should set description on input change', () => {
+  const newValue = 'new description'
+  const wrapper = shallow(<ExpenseForm></ExpenseForm>)
+  wrapper
+    .find('input')
+    .at(0)
+    .simulate('change', {
+      target: { value: newValue },
+    })
+  expect(wrapper.state('description')).toBe(newValue)
+})
+
+test('Should set note on textarea change', () => {
+  const newValue = 'new note'
+  const wrapper = shallow(<ExpenseForm></ExpenseForm>)
+  wrapper.find('textarea').simulate('change', {
+    target: { value: newValue },
+  })
+  expect(wrapper.state('note')).toBe(newValue)
+})
+
+test('Should set amount on its input change', () => {
+  const newValue = '12.12'
+  const wrapper = shallow(<ExpenseForm></ExpenseForm>)
+  wrapper
+    .find('input')
+    .at(1)
+    .simulate('change', {
+      target: { value: newValue },
+    })
+  expect(wrapper.state('amount')).toBe(newValue)
+})
+
+test('Should ignore change to amount with bad value', () => {
+  const newValue = '1.212'
+  const wrapper = shallow(<ExpenseForm></ExpenseForm>)
+  wrapper
+    .find('input')
+    .at(1)
+    .simulate('change', {
+      target: { value: newValue },
+    })
+  expect(wrapper.state('amount')).toBe('')
+})
+
+test('should call onSubmit prop on valid form submission', () => {
+  const onSubmitSpy = jest.fn()
+
+  let testEx = expenses[0]
+  delete testEx.id
+
+  const wrapper = shallow(
+    <ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy}></ExpenseForm>
+  )
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {},
+  })
+  expect(onSubmitSpy).toHaveBeenCalledWith(testEx)
+})
+
+test('should set new date on dateChange', () => {
+  const wrapper = shallow(<ExpenseForm></ExpenseForm>)
+  const now = moment()
+  wrapper.find('SingleDatePicker').prop('onDateChange')(now)
+  expect(wrapper.state('createdAt')).toEqual(now)
 })
