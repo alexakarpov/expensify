9fb5ea4574eb04b7ea952f26bc61bdf1fe713477
Alexandre Karpov <alexakarpov@gmail.com>
2021-09-10 09:41:19 -0400

    allow for 4 months to show

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -76,7 +76,7 @@ export default class ExpenseForm extends React.Component {
             onDateChange={this.onDateChange}
             focused={this.state.calendarFocused}
             onFocusChange={this.onFocusChange}
-            numberOfMonths={1}
+            numberOfMonths={4}
             isOutsideRange={() => false}
           />
           <textarea
