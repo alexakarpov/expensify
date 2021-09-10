b3d188490be0baa5d2b44a609de3312eb6e4409b
Alexandre Karpov <alexakarpov@gmail.com>
2021-09-09 22:43:03 -0400

    cleanup manual demo-hacks

--- src/app.js
+++ src/app.js
@@ -4,14 +4,12 @@ import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
-import getVisibleExpenses from './selectors/expenses'
-import moment from 'moment'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
+const state = store.getState()
 
 store.dispatch(
   addExpense({ description: 'Water bill', amount: 4500, createdAt: 2000 })
@@ -20,13 +18,6 @@ store.dispatch(addExpense({ description: 'Gas bill', createdAt: 3000 }))
 store.dispatch(
   addExpense({ description: 'Rent', amount: 109500, createdAt: 4000 })
 )
-
-const state = store.getState()
-console.log('all expenses:', state.expenses)
-console.log(state.filters)
-console.log('^active filters')
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log('visible expenses:', visibleExpenses)
 
 const jsx = (
   <Provider store={store}>
